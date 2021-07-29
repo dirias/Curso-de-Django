bb15ed6781299401b044ba742d0536e9c171f2d7
Didier Irias Méndez <didier.irias@gmail.com>
2021-07-29 17:19:10 -0600

    Bug fixed

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=False)
     password = models.CharField(max_length=100)
 
     first_name = models.CharField(max_length=100)
