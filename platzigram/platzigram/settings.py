1b29bd3412df70c01560cc1ab95e2beeb44c361a
Didier Irias <didier.irias@gmail.com>
2021-07-31 20:39:23 -0600

    Models completed

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 
     #local apps
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,30 +1,18 @@
-"""
-    Posts models.
-"""
-#Django
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """Post model."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 
-class User(models.Model):
-    email = models.EmailField(unique=False)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    is_admin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-    #RETO 1
-    city = models.CharField(max_length=100, blank=True)
-    country = models.CharField(max_length=100, blank=True)
-
     def __str__(self):
-        """Return email."""
-        return self.email
+        """Return title and username"""
+        return f'{self.title} by @ {self.use.username}'
