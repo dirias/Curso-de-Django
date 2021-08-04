9afbcf1bf1cf196239d8083efc73c34ace4c7f8f
Didier Irias <didier.irias@gmail.com>
2021-08-04 17:59:31 -0600

    Update user information completed

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -8,10 +8,30 @@ from django.contrib.auth.models import User
 from users.models import Profile
 # Exeptions
 from django.db.utils import IntegrityError
+# Forms
+from users.forms import ProfileForm
 # Create your views here.
 def update_profile(request):
     """Update a user's profile view"""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+        
+    return render(request=request, 
+        template_name='users/update_profile.html', 
+        context={'profile': profile, 
+                    'user': request.user, 
+                    'form': form})
     
 def login_view(request):
     """Login view"""
